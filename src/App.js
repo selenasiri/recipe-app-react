8ee52f0022abf47019b9c77ff650f530532bb8ae
Selena Siri <selenasiri@gmail.com>
2020-11-04 20:36:44 -0500

    search bar query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Axios from 'axios'
 import './App.css';
 
-function App() {
+const App = () => {
+const [query, setQuery] = useState("")
+
+const APP_ID = "e83f5706";
+
+const APP_KEY = "70192e6a2ff0d13d19134f3980848eca";
+
+const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+const getData = async () => {
+  const result = await Axios.get(url);
+
+  console.log(result)
+}
+
+const onChange = (e) => {
+setQuery(e.target.value);
+}
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  getData();
+}
+
   return (
-    <div>
-      <h1>Food Searching App</h1>
+    <div className="App">
+      <h1 onClick={getData}>Food Searching App</h1>
+      <form className="search-form" onSubmit={onSubmit}>
+        <input type="text" placeholder="Search Food" autoComplete="off" onChange={onChange} />
+        <input type="text" type="submit" alue="search" />
+      </form>
     </div>
   );
 }
