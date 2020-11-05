fd9bf5b4621324164f55b6813766de80e46d5865
Selena Siri <selenasiri@gmail.com>
2020-11-04 20:57:52 -0500

    recipe details

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import RecipeDetails from './RecipeDetails'
 
-const Recipe = () => {
+const Recipe = ({recipe}) => {
+  const {label, image, url, ingredients} = recipe.recipe;
   return (
-    <div>
-      <h2>Recipe</h2>
+    <div className="recipe">
+      <h2>{label}</h2>
+      <img src={image} alt={label} />
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        URL
+      </a>
+      <button>Ingredients</button>
+      <RecipeDetails />
     </div>
   )
 }
