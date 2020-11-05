705deb2bcad05bcf6fb9cfe5f488955952fafb44
Selena Siri <selenasiri@gmail.com>
2020-11-04 21:11:50 -0500

    alert 1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Axios from 'axios'
 import {v4 as uuidv4} from 'uuid'
 import './App.css';
 import Recipe from './components/Recipe'
+import Alert from './components/Alert'
 
 const App = () => {
-const [query, setQuery] = useState("")
-const [recipes, setRecipes] = useState([])
+const [query, setQuery] = useState("");
+const [recipes, setRecipes] = useState([]);
+const [alert, setAlert] = useState("");
 
 const APP_ID = "e83f5706";
 
@@ -15,15 +17,19 @@ const APP_KEY = "70192e6a2ff0d13d19134f3980848eca";
 const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 const getData = async () => {
-  const result = await Axios.get(url);
-setRecipes(result.data.hits)
-  console.log(result);
-  setQuery("");
-}
+  if(query !== "") {
+    const result = await Axios.get(url);
+    setRecipes(result.data.hits)
+   console.log(result);
+    setQuery("");
+  } else {
+    setAlert('Please fill the form')
+  }
+};
 
 const onChange = e => {
 setQuery(e.target.value);
-}
+};
 
 const onSubmit = e => {
   e.preventDefault();
@@ -34,6 +40,7 @@ const onSubmit = e => {
     <div className="App">
       <h1 onClick={getData}>Food Searching App</h1>
       <form className="search-form" onSubmit={onSubmit}>
+        {alert !=="" && <Alert alert={alert} />}
         <input 
         type="text" 
         placeholder="Search Food" 
